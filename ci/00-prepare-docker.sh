5812e12467f6daf78a78de40b2b356bd182055d6
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-03-25 14:44:48 +0100

    makeinfo is in texinfo, not in texi2html

--- ci/00-prepare-docker.sh
+++ ci/00-prepare-docker.sh
@@ -4,13 +4,12 @@
 # barely usable. And anyway we don't care: there's no software that         
 # we want to pull from these repos anyway.                                  
 find /etc/apt/sources.list.d/ -type f | xargs -r grep -li intel | xargs -r rm
-apt-get update -qq && apt-get install -qq build-essential git libmpfr-dev autoconf libtool texi2html
+apt-get update -qq && apt-get install -qq build-essential git libmpfr-dev autoconf libtool texinfo
 # the default MPFR library does not contain new C23 functions
 # we thus install the development version of MPFR
 git clone https://gitlab.inria.fr/mpfr/mpfr.git
 cd mpfr
 autoreconf -i
-./autogen.sh
 ./configure
 make -j
 make install
